47b5c26b6a218c02be8b6e027b80a0169108853b
laurentiuciuhu <33173001+laurentiuciuhu@users.noreply.github.com>
2021-05-17 07:25:14 +0200

    Add files via upload

---
+++ Curs 1/e-com/script.js
@@ -0,0 +1,68 @@
+new Vue({
+    el: '#products',
+    data: {
+        productsArray: [{
+            "id": "532",
+            "name": "PERFECTION KILLS DREAMS – T-SHIRT",
+            "deion": "What if you pursued perfection like you pursued the sun’s rays: every day but getting too close will get you in trouble?",
+            "price": "125",
+            "image_title": "mockup-75d95c01.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/08/mockup-75d95c01.jpg"
+          },
+          {
+            "id": "530",
+            "name": "TAKE THE RISK – T-SHIRT",
+            "deion": "What if you took the risk as calculated as someone trying to get away with a crime?",
+            "price": "99",
+            "image_title": "mockup-53bec5bc.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/08/mockup-53bec5bc.jpg"
+          },
+          {
+            "id": "516",
+            "name": "OPEN YOUR MIND – MUG",
+            "deion": "What if you opened your mind for new opportunities as a 3-year-old is open for learning?",
+            "price": "95",
+            "image_title": "mockup-570317f2.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/09/mockup-570317f2.jpg"
+          },
+          {
+            "id": "514",
+            "name": "ARE YOU PRODUCTIVE OR JUST BUSY – MUG",
+            "deion": "What if you just focused your entire energy the way a laser focuses its energy to cut through steel?",
+            "price": "68",
+            "image_title": "mockup-2f5bd762.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/09/mockup-2f5bd762.jpg"
+          },
+          {
+            "id": "520",
+            "name": "THINK OUT OF THE BOX – SWEATSHIRT",
+            "deion": "What if you though out of the box, as open and enthusiastic as a child unwrapping presents on Christmas day?",
+            "price": "65",
+            "image_title": "mockup-a60bdc59.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/08/mockup-a60bdc59.jpg"
+          },
+          {
+            "id": "518",
+            "name": "BELIEVE MEANS ACHIEVE – CUFFED BEANIE",
+            "deion": "What if you believed in yourself the way Zlatan Ibrahimovic believes in his powers?",
+            "price": "65",
+            "image_title": "mockup-3f7b54aa.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/09/mockup-3f7b54aa.jpg"
+          },
+          {
+            "id": "500",
+            "name": "KEEP WORKING HARD – SWEATSHIRT",
+            "deion": "What if you worked so hard for your dreams and goals like the Egyptians used to work for their pyramids?",
+            "price": "60",
+            "image_title": "mockup-b2db22cd.jpg",
+            "image": "https://entreprewear.co/wp-content/uploads/2020/08/mockup-b2db22cd.jpg"
+          }
+        ],
+        value: 0
+    },
+    computed: {
+        filter() {
+            
+        }
+    }
+})
